38bd72a20be884932ad4b1843a0b48ad8eb5d182
https://EngKen-Peter <kennedymbaluka23@gmail.com>
2025-07-25 02:05:17 +0300

    fix: only fetch all students for teachers in StudentList page

--- client/src/pages/StudentList.jsx
+++ client/src/pages/StudentList.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import StudentCard from "../components/StudentCard";
+import { useAuth } from '../contexts/AuthContext';
 
 const StudentList = () => {
   const [students, setStudents] = useState([]);
+  const { user, token } = useAuth();
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/students")
-      .then((res) => setStudents(res.data))
-      .catch((err) => console.error(err));
-  }, []);
+    if (user?.role === 'teacher') {
+      axios.get("/api/students", { headers: { Authorization: `Bearer ${token}` } })
+        .then((res) => setStudents(res.data))
+        .catch((err) => console.error(err));
+    }
+  }, [user, token]);
+
+  if (user?.role !== 'teacher') {
+    return <p className="text-center text-gray-600">You do not have access to view all students.</p>;
+  }
 
   if (students.length === 0) {
     return <p className="text-center text-gray-600">No students found.</p>;
